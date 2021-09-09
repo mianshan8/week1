4221d77ff745aa2bb79962009a15a9724ea1efe7
Nan <nanliu@umass.edu>
2021-09-09 13:58:42 -0400

    Week1

---
+++ week1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week1
+//
+//  Created by 劉楠 on 2021/09/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ week1Tests/week1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  week1Tests.swift
+//  week1Tests
+//
+//  Created by 劉楠 on 2021/09/09.
+//
+
+import XCTest
+@testable import week1
+
+class week1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
